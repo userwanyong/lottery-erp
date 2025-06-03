93d858588f7b54e62deac66c0214b73072558f0e
userwanyong <2026804718@qq.com>
2025-06-03 19:26:36 +0800

    feat: init project

---
+++ src/pages/UserOrder/index.tsx
@@ -0,0 +1,56 @@
+import { userOrder } from '@/services/ant-design-pro/api';
+import { PageContainer, ProColumns, ProTable } from '@ant-design/pro-components';
+import React from 'react';
+
+const UserOrder: React.FC = () => {
+  const columns: ProColumns<API.UserOrderItem>[] = [
+    {
+      title: '用户ID',
+      dataIndex: 'userId',
+      valueType: 'textarea',
+    },
+    {
+      title: '活动ID',
+      dataIndex: 'activityId',
+      valueType: 'textarea',
+    },
+    {
+      title: '活动名称',
+      dataIndex: 'activityName',
+      valueType: 'textarea',
+    },
+    {
+      title: '抽奖策略',
+      dataIndex: 'strategyId',
+      valueType: 'textarea',
+    },
+    {
+      title: '订单ID',
+      dataIndex: 'orderId',
+      valueType: 'textarea',
+    },
+    {
+      title: '下单时间',
+      dataIndex: 'orderTime',
+      valueType: 'textarea',
+    },
+    {
+      title: '下单时间',
+      dataIndex: 'orderTime',
+      valueType: 'textarea',
+    },
+    {
+      title: '订单状态',
+      dataIndex: 'orderState',
+      valueType: 'textarea',
+    },
+  ];
+
+  return (
+    <PageContainer>
+      <ProTable<API.UserOrderItem, API.PageParams> request={userOrder} columns={columns}></ProTable>
+    </PageContainer>
+  );
+};
+
+export default UserOrder;
